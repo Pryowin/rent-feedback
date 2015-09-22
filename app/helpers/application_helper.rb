b0f79eb11cfb304b28aefa5dfb532e4504b89718
Pryowin <dburke@amberfire.net>
2015-09-22 14:46:35 -0700

    Tweaked layout and tests for static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def complete_title(page_title = '')
+    base_title = RentFeedback::Application::APP_NAME
+    if page_title.empty?
+      return base_title
+    else
+      return "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,25 +2,31 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-
+  def setup
+    @title = RentFeedback::Application::APP_NAME
+  end
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", @title
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | #{@title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@title}"
   end
 
 end
