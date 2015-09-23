765bcec6ff6af137d852ef2e265ca419e1dcd427
Pryowin <dburke@amberfire.net>
2015-09-23 15:47:28 -0700

    Working Log in and Log Out

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get 'about'      => 'static_pages#about'
   get 'contact'    => 'static_pages#contact'
 
+  resources :users
+
+  # delete 'sign_out' => 'devise/sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
