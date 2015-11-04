a7caeddcb6c495c6da8a3b00db726b050fdab0db
Pryowin <dburke@amberfire.net>
2015-11-04 09:57:56 -0800

    Change application name

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module RentFeedback
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    APP_NAME = 'RentWrite'
+    APP_NAME = 'RentTalk'
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
