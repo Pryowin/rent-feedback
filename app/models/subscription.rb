5939833e4df0cc8b7424f35ed0a78a92504460b4
Pryowin <dburke@amberfire.net>
2015-11-10 13:37:36 -0800

    Tests for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,28 @@ class Subscription < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :user, presence: true
+
+  validate :allow_one_and_only_one_subscription_type
+
+  def allow_one_and_only_one_subscription_type
+    if building_id.nil? && city.nil? && postal_code.nil?
+      errmsg = 'Must provide city or postal code for a subscription'
+      errors.add(:city,errmsg)
+      errors.add(:postal_code,errmsg)
+    end
+
+    errmsg = 'Select just one item for a subscription'
+
+    unless building_id.nil?
+      errors.add(:city,errmsg) unless city.nil?
+      errors.add(:postal_code,errmsg) unless postal_code.nil?
+    end
+
+    if building_id.nil? && !city.nil? && !postal_code.nil?
+      errors.add(:city,errmsg)
+      errors.add(:postal_code,errmsg)
+    end
+
+  end
+
 end
--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @subscription = subscriptions(:one)
+  end
+
+  test 'User id cannot be blank' do
+    @subscription.user_id = nil
+    assert_not @subscription.valid?
+  end
+
+  test 'One of building/city/zip must be specifiied' do
+    @subscription.building_id = nil
+    @subscription.city = nil
+    @subscription.postal_code = nil
+    assert_not @subscription.valid?
+  end
+
+  test 'valid if one is provided' do
+    @subscription.building_id = 1
+    assert @subscription.valid?, 'Building Provided'
+    @subscription.building_id = nil
+    @subscription.city = 'City'
+    assert @subscription.valid?, 'City Provided'
+    @subscription.city = nil
+    @subscription.postal_code = '99999'
+    assert @subscription.valid?, 'Postal Code Provided'
+  end
+
+  test 'not valid if building and city provided' do
+    @subscription.city = 'City'
+    assert_not @subscription.valid?
+  end
+  test 'not valid if building and zip provided' do
+    @subscription.postal_code = '99999'
+    assert_not @subscription.valid?
+  end
+  test 'not valid if zip and city provided' do
+    @subscription.city = 'City'
+    @subscription.postal_code = '99999'
+    @subscription.building_id = nil
+    assert_not @subscription.valid?
+  end
+
+  test 'New subscriptions are created active' do
+    subscription = Subscription.new
+    subscription.user_id = @subscription.user_id
+    subscription.city = 'test'
+    subscription.save
+    check_sub = Subscription.find_by city: 'test'
+    assert_equal check_sub.active, true
+  end
+
+
 end
